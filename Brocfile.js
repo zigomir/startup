43767ca029ab76948cb02c0c8c532b4071487533
zigomir <zigomir@gmail.com>
2014-08-16 11:29:07 +0200

    such commit

---
+++ Brocfile.js
@@ -0,0 +1,27 @@
+var filterCoffeeScript = require('broccoli-coffee');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
+var less = require('broccoli-less');
+var env = require('broccoli-env').getEnv();
+var uglifyJavaScript = require('broccoli-uglify-js');
+
+var appCompiled = filterCoffeeScript('app');
+var stylesCompiled = less('app/styles');
+
+var appFiles = pickFiles(appCompiled, {
+    srcDir: '/',
+    files: ['**/*.js'],
+    destDir: 'src'
+});
+
+var styleFiles = pickFiles(stylesCompiled, {
+    srcDir: '/',
+    files: ['**/*.css'],
+    destDir: 'css'
+});
+
+if (env === 'production') {
+  appFiles = uglifyJavaScript(appFiles);
+}
+
+module.exports = mergeTrees([appFiles, styleFiles, 'public']);
---
+++ README.md
@@ -0,0 +1,27 @@
+### Setup
+
+Prerequisites
+
+```sh
+npm install -g bower
+npm install -g broccoli-cli
+```
+
+```sh
+npm install
+bower install # or bower install -S react
+```
+
+### Running
+
+```sh
+broccoli serve
+open http://localhost:4200
+```
+
+### Build and deploy - edit deploy script first
+
+
+```sh
+./deploy
+```
